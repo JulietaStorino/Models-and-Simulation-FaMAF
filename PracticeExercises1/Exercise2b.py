b24e107f602b6c3fdb6ae03dc5c56c41ac8d216c
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-04-21 04:57:54 -0300

    Exercise 2b implemented

---
+++ PracticeExercises1/Exercise2b.py
@@ -0,0 +1,19 @@
+# Exercise 2b - Practice Exercises 3: Random numbers and Monte Carlo method
+from random import random
+from Chapter4 import *
+
+def simulation(): # 1 Win the game - 0 Fail the game 
+    if random() < .5:
+        X = random()+random()
+    else:
+        X = random()+random()+random()
+    return X>=1
+
+if __name__ == "__main__":
+    nvalues = (100,1000,10000,100000,1000000)
+    for n in nvalues:
+        estimation = 0
+        for _ in range(n):
+            estimation += simulation()
+        estimation /= n
+        print(f'Case: n = {n}. P[X>=1] ≈ {estimation}.')

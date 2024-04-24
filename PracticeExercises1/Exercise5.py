84da698d49a5b7528b68b671360fd4d9918609cb
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-04-24 07:13:11 -0300

    Exercise 5 implemented

---
+++ PracticeExercises1/Exercise5.py
@@ -0,0 +1,59 @@
+from random import random
+import numpy as np
+import math
+import Chapter5 as ch
+
+def a(x):
+    return (1 - x**2) ** (3/2) 
+
+def b(x):
+    return x / (x** 2 - 1)
+
+def c(x):
+    return x * (1 + x**2)**(-2)
+
+def d(x):
+    return 2 * np.exp(-x**2)
+
+def e(x,y):
+    return np.exp((x+y)**2)
+
+def I(y,x): #I_y(x)
+    return y < x
+
+def f(x,y):
+    return np.exp(-(x+y)) * I(y,x)
+
+if __name__ == "__main__":
+    iterations = [100, 1000, 10000, 100000, 1000000]
+    print('Estimations:\n')
+    #5a
+    print(f'\ta:')
+    for iteration in iterations:
+        Estimation = ch.MonteCarlo_0_1(a, iteration)
+        print(f'\t\t {iteration} iterations: {Estimation}')
+    #5b
+    print(f'\tb:')
+    for iteration in iterations:
+        Estimation = ch.MonteCarlo_a_b(b, 2, 3, iteration)
+        print(f'\t\t {iteration} iterations: {Estimation}')
+    #5c
+    print(f'\tc:')
+    for iteration in iterations:
+        Estimation = ch.MonteCarlo_0_inf(c, iteration)
+        print(f'\t\t {iteration} iterations: {Estimation}')
+    #5d
+    print(f'\td:')
+    for iteration in iterations:
+        Estimation = ch.MonteCarlo_0_inf(d, iteration)
+        print(f'\t\t {iteration} iterations: {Estimation}')
+    #5e
+    print(f'\te:')
+    for iteration in iterations:
+        Estimation = MultipleMonteCarlo_0_1(e, iteration)
+        print(f'\t\t {iteration} iterations: {Estimation}')
+    #5f
+    print(f'\tf:')
+    for iteration in iterations:
+        Estimation = MultipleMonteCarlo_0_inf(f, iteration)
+        print(f'\t\t {iteration} iterations: {Estimation}')

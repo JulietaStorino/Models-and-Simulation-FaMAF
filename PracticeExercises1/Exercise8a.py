bcbfa5efbcb0386acf2393830ec64241875e6db0
Juli Storino <julieta_storino@mi.unc.edu.ar>
2024-04-24 10:04:23 -0300

    Exercise 8a implementation

---
+++ PracticeExercises1/Exercise8a.py
@@ -0,0 +1,20 @@
+from random import random
+from math import exp
+
+def CalculateN(Nsim):
+    estimation = 0
+    for _ in range(Nsim):
+        U = random()
+        N = 1
+        while U > exp(-3):
+            U *= random()
+            N += 1
+        estimation += N
+    return estimation/Nsim
+
+if __name__ == "__main__":
+    iterations = [100, 1000, 10000, 100000, 1000000]
+    for iteration in iterations:
+        estimation = CalculateN(iteration)
+        print(f'Iterations: {iteration}.')
+        print(f'Estimation obtained: {estimation}.\n')
